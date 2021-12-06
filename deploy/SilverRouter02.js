ffa39ee53e611827c94a7d7a3d1ee21157dde12a
Analyst <silverswaporg@protonmail.com>
2021-12-06 22:29:51 +0800

    Added new Router contract address to README.me

--- deploy/SilverRouter02.js
+++ deploy/SilverRouter02.js
@@ -5,7 +5,7 @@ module.exports = async function ({ getNamedAccounts, deployments }) {
 
   let wethAddress = "0xDd7FBd7e655DE4B8eccb2B3254F6B69B569F0A9a"; // This is for testnet
   // WCRO is 0x5C7F8A570d578ED84E63fdFA7b1eE72dEae1AE23
-  // WTCRO is 
+  // WTCRO is 0xDd7FBd7e655DE4B8eccb2B3254F6B69B569F0A9a
   // Actually.. Wrapped BNB or CRO
   // https://testnet.bscscan.com/address/0xae13d989dac2f0debff460ac112a837c89baa7cd
   // https://cronos.crypto.org/explorer/address/0x5C7F8A570d578ED84E63fdFA7b1eE72dEae1AE23
